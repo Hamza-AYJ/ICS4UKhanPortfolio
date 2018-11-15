715a8a32eb9623af5fa246c4d4e0fcb5b9d9b1de
Hamza-AYJ <36042600+Hamza-AYJ@users.noreply.github.com>
2018-11-15 10:58:49 -0500

    Create WhatsForDinner.js

---
+++ WhatsForDinner.js
@@ -0,0 +1,14 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill(255, 255, 255);
+ellipse(150,200,175,175);
+fill(250, 225, 5);
+ellipse(150, 180, 75,75);
+
+fill(255, 157, 0);
+rotate(4);
+//giant dorito
+triangle(210, 320, 320, 330, 280,10);
+
