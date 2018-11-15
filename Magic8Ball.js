8095876e4e188f726a44cc00f2e92ec84b4f993f
Hamza-AYJ <36042600+Hamza-AYJ@users.noreply.github.com>
2018-11-15 11:01:29 -0500

    Create Magic8Ball.js

---
+++ Magic8Ball.js
@@ -0,0 +1,24 @@
+fill(0, 0, 0);
+ellipse(200, 200, 375, 375);
+fill(60, 0, 255);
+triangle(200, 104, 280, 280, 120, 280);
+fill(255, 255, 255);
+var answer = floor(random(1, 6));
+
+if (answer === 1) {
+    text("1", 176, 200);
+}
+else if(answer === 2){
+    text("2", 159, 229); 
+
+}
+else if(answer === 3){
+    text("3", 159, 229); 
+
+}else if(answer === 4){
+    text("4", 159, 229); 
+
+}else if (answer ===5){
+    text("5", 159, 229); 
+
+}
