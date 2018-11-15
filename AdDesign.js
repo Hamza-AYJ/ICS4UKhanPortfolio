68bfbc1d0e2766a84c917ab2ee138a86491cceb6
Hamza-AYJ <36042600+Hamza-AYJ@users.noreply.github.com>
2018-11-15 11:01:01 -0500

    Create AdDesign.js

---
+++ AdDesign.js
@@ -0,0 +1,18 @@
+fill(255, 0, 0);
+textSize(30);
+text("Flex Seal!", 10, 30);
+textSize(11);
+text("The Flex Seal can seal virtually anything, it can make your bed waterproof!", 10, 87);
+
+textSize(11);
+text("The flex seal is the best product to ever exist.", 10, 100);
+
+fill(99, 78, 99);
+rect(165, 231,69,151);
+fill(224, 215, 36);
+rect(165, 231, 69, 30);
+fill(9, 152, 219);
+rect(192, 207, 14, 24);
+rotate(90);
+textSize(30);
+text("FLEX SEAL", 160, 281);
